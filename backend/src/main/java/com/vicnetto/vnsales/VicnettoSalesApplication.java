da54b10bd9c0a6cb8ba816accf69a5367771df4d
vicnetto <victorhmnetto@gmail.com>
2021-05-26 17:23:15 -0300

    :tada: Starting the sales project
    This project will be a sales simulation using Spring Boot and React. All
    the steps to make this project work is found on the README.md file.

---
+++ backend/src/main/java/com/vicnetto/vnsales/VicnettoSalesApplication.java
@@ -0,0 +1,13 @@
+package com.vicnetto.vnsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicnettoSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicnettoSalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vicnetto/vnsales/VicnettoSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicnetto.vnsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VicnettoSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
