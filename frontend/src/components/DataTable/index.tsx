49b3cb967227d6c2561e36d9a242aa878bf248ed
vicnetto <victorhmnetto@gmail.com>
2021-05-28 16:37:16 -0300

    :art: :sparkles: Adding some basic static components
    The web page is starting to get a shape. For that it was used some basic
    static components, like table, footer and navegation bar.

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,118 @@
+const DataTable = () => {
+    return (
+        <div className="container">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Seller</th>
+                        <th>Customers Visited</th>
+                        <th>Closed Deals</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>26/01/2000</td>
+                        <td>Victor Netto</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>12345.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+  }
+
+ export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://github.com/vicnetto" target="_blank" rel="noreferrer">Victor Netto</a></p>
+                <p className="text-light"><strong>Sales Simulator</strong></p>
+            </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
